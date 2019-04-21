4cae801a5596fffaecdd4e7ad29b572943a4c1ab
Octavian Ruda <octavian.ruda@emag.ro>
2019-04-21 11:38:09 +0300

    Bug fixing and improvements

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-let Hap;
-let Device = require('./lib/device');
-let Storage  = require('./lib/storage');
+let Homebridge;
+let Device  = require('./lib/device');
+let Storage = require('./lib/storage');
+
+const PLUGIN_NAME   = 'homebridge-samsung-tizen';
+const PLATFORM_NAME = 'SamsungTizen';
 
 module.exports = (homebridge) => {
-    Hap = homebridge.hap;
-
-    homebridge.registerPlatform('homebridge-samsung-tizen', 'SamsungTizen', SamsungPlatform);
+    Homebridge = homebridge;
+    Homebridge.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, SamsungPlatform);
 }
 
 class SamsungPlatform {
     constructor(log, config, api) {
-        this.log = log;
-        this.api = api;
+        if (!config) { return; }
 
+        this.log     = log;
+        this.api     = api;
         this.storage = new Storage(api);
-        this.devices = config['devices'] || [];
 
         this.config = {
             delay   : config.delay,
             inputs  : config.inputs || [],
+            devices : config.devices || [],
             method  : config.method,
             refresh : config.refresh,
             timeout : config.timeout
@@ -30,8 +33,8 @@ class SamsungPlatform {
     accessories(callback) {
         let accessories = [];
 
-        for (let device of this.devices) {
-            device = new Device(this, Hap, device);
+        for (let device of this.config.devices) {
+            device = new Device(this, Homebridge.hap, device);
 
             // Add the new device accessories to the list
             accessories = [
