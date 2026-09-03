96edc2b9d19f6eef6957417d9ba1e87ad104a0b8
Octavian Ruda <lordtavicu@gmail.com>
2026-09-03 19:29:06 +0300

    Image zoom

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,34 @@
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import mediumZoom from 'medium-zoom'
 import SmartThingsToken from '../components/SmartThingsToken.vue'
+import './custom.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('SmartThingsToken', SmartThingsToken)
+  },
+  setup() {
+    const route = useRoute()
+    let zoom
+
+    const initZoom = () => {
+      zoom?.detach()
+      zoom = mediumZoom('.main img', {
+        margin: 30,
+        background: 'var(--vp-c-bg)'
+      })
+    }
+
+    onMounted(() => {
+      initZoom()
+    })
+
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
   }
 }
